b4baafa421ff654f7936ffff841d08c3bd30f545
Trang Vu <Trang.Vu3@metropolia.fi>
2025-06-21 15:54:07 +0300

    add anchor link to menu bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,28 @@ import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 import { useState } from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
+import { useRef } from 'react'
 
 const Navbar = () => {
     const[menu, setMenu] = useState("home");
+    const menuRef = useRef();
+
+    const openMenu = () => {
+        menuRef.current.style.right="0";
+    }
+    const closeMenu = () => {
+        menuRef.current.style.right="-350px";
+    }
+
     return (
         <div className="navbar">
             <img src={logo} alt="" />
-            <ul className='nav-menu'>
-                <li><AnchorLink  className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline} alt=''/> : <> </> }</li>
+            <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open'/>
+            <ul ref={menuRef} className='nav-menu'>
+                <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
+                <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline} alt=''/> : <> </> }</li>
                 <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline} alt=''/> : <> </> }</li>
                 <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={underline} alt=''/> : <> </> }</li>
                 <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu==="work"?<img src={underline} alt=''/> : <> </> }</li>
